c30cd06a7805aeaefd1532e36d04c79ebf4885d5
napro1299 <56209602+napro1299@users.noreply.github.com>
2020-02-10 19:29:31 -0500

    added embed command

---
+++ commands/random/embed.js
@@ -0,0 +1,34 @@
+const Commando = require('discord.js-commando');
+const { RichEmbed } = require('discord.js');
+
+class EmbedCommand extends Commando.Command {
+    constructor(client) {
+        super(client, {
+            name: 'embed',
+            group: 'random',
+            memberName: 'embed',
+            description: 'Embed text',
+            examples: ['This is an embed.'],
+            args: [
+                {
+                    key: 'text',
+                    prompt: 'Text to embed',
+                    type: 'string'
+                }
+            ]
+        });
+    }
+
+    run(msg, args) {
+        const { text } = args;
+        const embed = new RichEmbed()
+            .setDescription(text)
+            .setAuthor(msg.author.username, msg.author.displayAvatarURL)
+            .setColor(0x00AE86)
+            .setTimestamp();
+
+        return msg.embed(embed);
+    }
+}
+
+module.exports = EmbedCommand;
